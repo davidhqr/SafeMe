34306d41da234f9990f4a345b84812f0ced97157
David Zhang <davidhqr@gmail.com>
2018-05-05 19:30:12 -0400

    Add request-promise

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
+var request = require('request-promise');
 
 app.use(express.static(__dirname));
 app.listen(3000);
+
+request({
+    "method": "GET",
+    "uri": "https://api.github.com/",
+    "json": true,
+    "headers": {
+        "User-Agent": "My little demo app"
+    }
+}).then(console.log, console.log);
 
